4198c41b0d607d88b112617a980b7c024effdc5d
tangbc_ <15760666366@163.com>
2025-08-24 20:59:20 +0800

    首页

---
+++ frontend/src/views/login/components/forget_password.vue
@@ -0,0 +1,92 @@
+<template>
+    <el-dialog v-model="state.forgetPasswordDialog" title="忘记密码" width="400px">
+        <el-form class="login-form" :label-position="lablePosition" :rules="rules">
+            <el-form-item label="输入您的注册账号" prop="account">
+                <el-input v-model="forgetData.account" placeholder="输入您的注册账号"/>
+            </el-form-item>
+            <el-form-item label="输入您的个人邮箱" prop="email">
+                <el-input v-model="forgetData.email" placeholder="输入您的个人邮箱"/>
+            </el-form-item>
+        </el-form>
+        <!-- 底部内容 -->
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="state.forgetPasswordDialog = false">取消</el-button>
+                <el-button type="primary" @click="openChangePassword">
+                    下一步
+                </el-button>
+                </div>
+        </template>
+    </el-dialog>
+<!-- 我是分割线 -->
+<!-- 我是分割线 -->
+<!-- 我是分割线 -->
+    <el-dialog v-model="state.changePasswordDialog" title="忘记密码" width="400px">
+        <el-form class="login-form" :label-position="lablePosition" :rules="rules">
+            <el-form-item label="输入您的新密码" prop="password">
+                <el-input v-model="forgetData.account" placeholder="输入您的新密码"/>
+            </el-form-item>
+            <el-form-item label="再次确认您的新密码" prop="repassword">
+                <el-input v-model="forgetData.email" placeholder="再次确认您的新密码"/>
+            </el-form-item>
+        </el-form>
+        <!-- 底部内容 -->
+        <template #footer>
+            <div class="dialog-footer">
+                <el-button @click="state.changePasswordDialog = false">取消</el-button>
+                <el-button type="primary" @click="state.changePasswordDialog = false">
+                    确定
+                </el-button>
+                </div>
+        </template>
+    </el-dialog>
+</template>
+
+<script lang="ts" setup>
+import { reactive,ref } from 'vue'
+
+const lablePosition=ref('top')
+
+interface forgetData{
+    account:number;
+    email:string;
+    password:string;
+
+}
+
+const forgetData:forgetData=reactive({
+    account:null,
+    email:'',
+})
+
+const rules = reactive({
+    account:[
+        { required: true, message: '请输入您的注册账号', trigger: 'blur' },
+    ],
+    email:[
+        { required: true, message: '请输入您的注册邮箱', trigger: 'blur' },
+    ]
+})
+
+const state=reactive({
+    forgetPasswordDialog:false,
+    changePasswordDialog:false,
+})
+
+const openChangePassword=()=>{
+    state.forgetPasswordDialog=false
+    state.changePasswordDialog=true
+}
+
+const open=()=>{
+    state.forgetPasswordDialog=true
+}
+
+defineExpose({
+    open
+})
+
+</script>
+
+<style lang="scss" scoped>
+</style>
--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -4,13 +4,52 @@
             <el-header class="header_wrapped">
                 <div class="header-content">
                     <h3>通用后台管理系统</h3>
-                    <span class=""welcome>欢迎您的登录</span>
+                    <span class="welcome">欢迎您的登录</span>
                 </div>
             </el-header>
             <el-main>
                 <div class="login-wrapped">
                     <el-card class="box-card">
-
+                        <el-tabs v-model="activeName" class="demo-tabs" :stretch="true">
+                            <el-tab-pane label="登录" name="first">
+                                <el-form class="login-form">
+                                    <el-form-item label="账号">
+                                        <el-input v-model="loginData.account" placeholder="请输入账号"/>
+                                    </el-form-item>
+                                    <el-form-item label="密码">
+                                        <el-input v-model="loginData.password" placeholder="请输入密码"/>
+                                    </el-form-item>
+                                    <!--底部外壳-->
+                                    <div class="footer-wrapped">
+                                        <div class="forget-password">
+                                            <span class="forget-password-button" @click="openForget">忘记密码</span>
+                                        </div>
+                                    </div>
+                                    <div class="footer-button">
+                                        <el-button type="primary">登录</el-button>
+                                    </div>
+                                    <div class="footer-go-register">
+                                        还没有账号？<span class="go-register">马上注册</span>
+                                    </div>
+                                </el-form>
+                            </el-tab-pane>
+                            <el-tab-pane label="注册" name="second">
+                                <el-form class="">
+                                    <el-form-item label="账号">
+                                        <el-input v-model="registerData.account" placeholder="账号长度6-12位"/>
+                                    </el-form-item>
+                                    <el-form-item label="密码">
+                                        <el-input v-model="registerData.password" placeholder="密码需长度6-12位含字母数字"/>
+                                    </el-form-item>
+                                    <el-form-item label="确认密码">
+                                        <el-input v-model="registerData.password" placeholder="请再次输入密码"/>
+                                    </el-form-item>
+                                    <div class="footer-button">
+                                        <el-button type="primary">注册</el-button>
+                                    </div>
+                                </el-form>
+                            </el-tab-pane>
+                        </el-tabs>
                     </el-card>
                 </div>
             </el-main>
@@ -26,9 +65,37 @@
             </el-footer>
     </el-container>
   </div>
+  <forget ref="forgetP"></forget>
 </template>
 
 <script lang="ts" setup>
+import { ref,reactive } from 'vue'
+import forget from './components/forget_password.vue'
+const activeName = ref('first')
+
+interface formData{
+    account:number;
+    password:string;
+    repassword ? :string;
+}
+
+const loginData:formData=reactive({
+    account:null,
+    password:'',
+})
+
+const registerData:formData=reactive({
+    account:null,
+    password:'',
+    repassword:'',
+})
+
+const forgetP=ref()
+
+const openForget=()=>{
+    forgetP.value.open()
+}
+
 </script>
 
 <style lang="scss" scoped>
@@ -49,7 +116,7 @@
         background-image:url('@/assets/code.png');
         height:600px;
         --el-main-padding:0;
-        
+
         .login-wrapped{
             width:1200px;
             height:600px;
@@ -61,6 +128,48 @@
                 float:right;
                 position:relative;
                 top:14%;
+
+                .login-form{
+                    .footer-wrapped{
+                        display:flex;
+                        flex-direction:column;
+                        .forget-password{
+                            display:flex;
+                            justify-content:flex-end;
+                            margin:10px;
+
+                            .forget-password-button{
+                                font-size:12px;
+                                color:#409eff;
+                                cursor:pointer;
+                            }
+
+                        }
+
+
+
+                        .footer-go-register{
+                            font-size:12px;
+                            margin:12px 0;
+                            display:flex;
+                            justify-content: center;
+
+                            .go-register{
+                                font-size:12px;
+                                color:#409eff;
+                                cursor:pointer;
+                            }
+                        }
+                    }
+
+                    .footer-button{
+                        width:100%;
+                        display:flex;
+                        justify-content:center;
+                    }
+
+                }
+
             }
         }
     }
@@ -83,4 +192,29 @@
             }
         }
     }
+
+    .el-form{
+        margin-top:30px;
+    }
+    
+    :deep(.el-tabs-item){
+        color:#333;
+        font-size:18px;
+    }
+
+    :deep(.el-input__inner){
+        height:40px;
+    }
+
+    :deep(.el-form-item__label){
+        height:40px;
+        line-height:40px;
+    }
+
+    :deep(.el-button){
+        width:300px;
+        height:45px;
+        font-size:16px;
+    }
+
 </style>
